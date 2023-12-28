75ac821c6edfe791046c12a6f40e40ba96cadb50
Abhinav Yadav <actcrick02@gmail.com>
2023-12-28 23:47:18 +0530

    push bootstrap code and finish project

--- django_portfolio_basic/settings.py
+++ django_portfolio_basic/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
